5c0cbd9af76d112e3a6454d9d8fe0da6b39a337c
codebyshoaib <shobbi800@gmail.com>
2024-08-24 19:41:26 +0500

    React Todo APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import './App.css'
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
 import TODO from "./TODO/TODO";
 function App() {
- return(
-  
-        <div className="appDiv">
-         <h1>Todo App</h1>
-         <TODO></TODO>
-         </div>
-    
- 
- );
+  return (
+    <div className="appDiv">
+      <h1>Todo App</h1>
+      <TODO></TODO>
+      <p>
+        Developed with <FontAwesomeIcon icon={faHeart} color="#A52A2A" /> by{" "}
+        <bold><span >Shoaib</span></bold>
+      </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
